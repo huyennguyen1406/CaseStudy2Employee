50082c60c352e85cc8eac75f54b25b3a7d9de87b
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-01-06 11:14:07 +0700

    add do while vao main

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,45 +12,52 @@ public class Main {
 
     public static void menuLogin() {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+
+        do {
             System.out.println("HỆ THỐNG ĐĂNG NHẬP");
             System.out.println("1. Đăng nhập");
             System.out.println("2. Đăng ký");
             System.out.println("3. Xóa tài khoản");
             System.out.println("4. Hiển thị tài khoản");
-            int choice = ChoiceRegex.getChoice();
-
-            switch (choice) {
-                case 1:
-                    int choice1 = AccountManager.signIn();
-                    if (choice1 == 1) {
-                        Main3.main2();
+            try {
+                int choice = ChoiceRegex.getChoice();
+                switch (choice) {
+                    case 1:
+                        int choice1 = AccountManager.signIn();
+                        if (choice1 == 1) {
+                            Main3.main2();
+                            break;
+                        } else if (choice1 == 2) {
+                            Main2.main1();
+                            break;
+                        } else {
+                            System.out.println("Tài khoản/mật khẩu không chính xác!!!");
+                            break;
+                        }
+                    case 2:
+                        try {
+                            AccountManager.addAccount();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                         break;
-                    } else if (choice1 == 2) {
-                        Main2.main1();
+                    case 3:
+                        try {
+                            AccountManager.removeAccount();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                         break;
-                    } else {
-                        System.out.println("Tài khoản/mật khẩu không chính xác!!!");
+                    case 4:
+                        AccountManager.show();
                         break;
-                    }
-                case 2:
-                    try {
-                        AccountManager.addAccount();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 3:
-                    try {
-                        AccountManager.removeAccount();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 4:
-                    AccountManager.show();
-                    break;
+                    default:
+                        System.out.println("Thử lại!!!");
+                }
+            } catch (Exception e) {
+                System.out.println("Nhập lỗi");
             }
         }
+        while (true);
     }
 }
